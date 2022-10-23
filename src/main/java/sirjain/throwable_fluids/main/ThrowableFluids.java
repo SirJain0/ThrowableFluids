d90d0790c7b5fe6de99fd158bd7cee78d927f0b0
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-10-23 15:09:14 -0400

    Get rid of logger

--- src/main/java/sirjain/throwable_fluids/main/ThrowableFluids.java
+++ src/main/java/sirjain/throwable_fluids/main/ThrowableFluids.java
@@ -1,18 +1,15 @@
 package sirjain.throwable_fluids.main;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import sirjain.throwable_fluids.items.ModItems;
 
 public class ThrowableFluids implements ModInitializer {
 	public static final String MOD_ID = "throwablefluids";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
 		ModItems.registerMobSpawnEggItems();
-		LOGGER.info("Hello Fabric world!");
+		System.out.println("Hi, "+MOD_ID+"!");
 	}
 }
