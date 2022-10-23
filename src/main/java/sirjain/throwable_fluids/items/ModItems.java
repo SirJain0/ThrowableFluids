b6206f336395a5ff643d08da97ae853d25c9507d
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-10-23 10:26:09 -0400

    Finish setting up environment

---
+++ src/main/java/sirjain/throwable_fluids/items/ModItems.java
@@ -0,0 +1,11 @@
+package sirjain.throwable_fluids.items;
+
+public class ModItems {
+    public static void registerModItems() {
+        // Register here
+    }
+
+    public static void registerMobSpawnEggItems() {
+        // Register here
+    }
+}
