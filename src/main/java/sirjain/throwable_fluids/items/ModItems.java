72bcfe7c09023bffd0cf342201e2886b901182b7
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-11-27 18:03:03 -0500

    Add custom item group

--- src/main/java/sirjain/throwable_fluids/items/ModItems.java
+++ src/main/java/sirjain/throwable_fluids/items/ModItems.java
@@ -18,34 +18,34 @@ public class ModItems {
     public static Item THROWABLE_LAVA;
 
     public static void registerModItems() {
-        WORM_CREAM = Registry.register(
-                Registry.ITEM,
-                new Identifier(ThrowableFluids.MOD_ID, "worm_cream"),
-                new Item(new Item.Settings().group(ItemGroup.MISC).fireproof())
-        );
-
-        SPLASHED_GEL = Registry.register(
-                Registry.ITEM,
-                new Identifier(ThrowableFluids.MOD_ID, "splashed_gel"),
-                new Item(new Item.Settings().group(ItemGroup.MISC).fireproof())
-        );
-
-        SPLASHER_FISH = Registry.register(
-                Registry.ITEM,
-                new Identifier(ThrowableFluids.MOD_ID, "splasher_fish"),
-                new Item(new Item.Settings().food(ModFoodComponents.SPLASHER_FISH).group(ItemGroup.MISC))
-        );
-
         THROWABLE_WATER = Registry.register(
                 Registry.ITEM,
                 new Identifier(ThrowableFluids.MOD_ID, "throwable_water"),
-                new ThrowableWaterItem(new Item.Settings().group(ItemGroup.MISC).maxCount(32))
+                new ThrowableWaterItem(new Item.Settings().group(ThrowableFluids.TF_ITEM_GROUP).maxCount(32))
         );
 
         THROWABLE_LAVA = Registry.register(
                 Registry.ITEM,
                 new Identifier(ThrowableFluids.MOD_ID, "throwable_lava"),
-                new ThrowableLavaItem(new Item.Settings().group(ItemGroup.MISC).maxCount(32))
+                new ThrowableLavaItem(new Item.Settings().group(ThrowableFluids.TF_ITEM_GROUP).maxCount(32))
+        );
+
+        WORM_CREAM = Registry.register(
+                Registry.ITEM,
+                new Identifier(ThrowableFluids.MOD_ID, "worm_cream"),
+                new Item(new Item.Settings().group(ThrowableFluids.TF_ITEM_GROUP).fireproof())
+        );
+
+        SPLASHED_GEL = Registry.register(
+                Registry.ITEM,
+                new Identifier(ThrowableFluids.MOD_ID, "splashed_gel"),
+                new Item(new Item.Settings().group(ThrowableFluids.TF_ITEM_GROUP).fireproof())
+        );
+
+        SPLASHER_FISH = Registry.register(
+                Registry.ITEM,
+                new Identifier(ThrowableFluids.MOD_ID, "splasher_fish"),
+                new Item(new Item.Settings().food(ModFoodComponents.SPLASHER_FISH).group(ThrowableFluids.TF_ITEM_GROUP))
         );
     }
 
@@ -53,13 +53,13 @@ public class ModItems {
         SPLASHER_SPAWN_EGG = Registry.register(
                 Registry.ITEM,
                 new Identifier(ThrowableFluids.MOD_ID, "splasher_spawn_egg"),
-                new SpawnEggItem(ModEntityTypes.SPLASHER_ENTITY, 0xaa9cff, 0xdcac55, new Item.Settings().group(ItemGroup.MISC))
+                new SpawnEggItem(ModEntityTypes.SPLASHER_ENTITY, 0xaa9cff, 0xdcac55, new Item.Settings().group(ThrowableFluids.TF_ITEM_GROUP))
         );
 
         GLOW_WORM_SPAWN_EGG = Registry.register(
                 Registry.ITEM,
                 new Identifier(ThrowableFluids.MOD_ID, "glow_worm_spawn_egg"),
-                new SpawnEggItem(ModEntityTypes.GLOW_WORM_ENTITY, 0xa73b3f, 0xff7c1b, new Item.Settings().group(ItemGroup.MISC))
+                new SpawnEggItem(ModEntityTypes.GLOW_WORM_ENTITY, 0xa73b3f, 0xff7c1b, new Item.Settings().group(ThrowableFluids.TF_ITEM_GROUP))
         );
     }
 }
