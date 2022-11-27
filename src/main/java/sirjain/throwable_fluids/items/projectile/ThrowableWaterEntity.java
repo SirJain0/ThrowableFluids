48b0bc14ac9f0ba96ed0868138927e2ca2259aec
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-11-27 10:48:23 -0500

    Get projectile to place water, remove client file

--- src/main/java/sirjain/throwable_fluids/items/projectile/ThrowableWaterEntity.java
+++ src/main/java/sirjain/throwable_fluids/items/projectile/ThrowableWaterEntity.java
@@ -3,6 +3,8 @@ package sirjain.throwable_fluids.items.projectile;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.Item;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
@@ -11,6 +13,8 @@ import sirjain.throwable_fluids.entity.other.ModEntityTypes;
 import sirjain.throwable_fluids.items.ModItems;
 
 public class ThrowableWaterEntity extends ThrownItemEntity {
+    public static final Fluid fluid = Fluids.WATER;
+
     public ThrowableWaterEntity(EntityType<? extends ThrownItemEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -32,6 +36,7 @@ public class ThrowableWaterEntity extends ThrownItemEntity {
     @Override
     protected void onBlockHit(BlockHitResult blockHitResult) {
         System.out.println("Hit a block!");
+        world.setBlockState(getBlockPos(), fluid.getDefaultState().getBlockState(), 11);
         super.onBlockHit(blockHitResult);
     }
 
