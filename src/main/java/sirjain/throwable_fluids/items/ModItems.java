d408d2bc88489f98625222c57367247ba3173e29
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-11-30 20:51:31 -0500

    Get rid of custom item group
    There are too few mod items to have a custom item group.

--- src/main/java/sirjain/throwable_fluids/items/ModItems.java
+++ src/main/java/sirjain/throwable_fluids/items/ModItems.java
@@ -25,37 +25,37 @@ public class ModItems {
         THROWABLE_WATER = Registry.register(
                 Registry.ITEM,
                 new Identifier(ThrowableFluids.MOD_ID, "throwable_water"),
-                new ThrowableWaterItem(new Item.Settings().group(ThrowableFluids.TF_ITEM_GROUP).maxCount(32))
+                new ThrowableWaterItem(new Item.Settings().group(ItemGroup.BREWING).maxCount(32))
         );
 
         THROWABLE_LAVA = Registry.register(
                 Registry.ITEM,
                 new Identifier(ThrowableFluids.MOD_ID, "throwable_lava"),
-                new ThrowableLavaItem(new Item.Settings().group(ThrowableFluids.TF_ITEM_GROUP).maxCount(32))
+                new ThrowableLavaItem(new Item.Settings().group(ItemGroup.BREWING).maxCount(32))
         );
 
         WORM_ESSENCE = Registry.register(
                 Registry.ITEM,
                 new Identifier(ThrowableFluids.MOD_ID, "worm_essence"),
-                new WormEssenceItem(new Item.Settings().group(ThrowableFluids.TF_ITEM_GROUP).fireproof())
+                new WormEssenceItem(new Item.Settings().group(ItemGroup.MISC).fireproof())
         );
 
         SPLASHED_GEL = Registry.register(
                 Registry.ITEM,
                 new Identifier(ThrowableFluids.MOD_ID, "splashed_gel"),
-                new Item(new Item.Settings().group(ThrowableFluids.TF_ITEM_GROUP))
+                new Item(new Item.Settings().group(ItemGroup.MISC))
         );
 
         SPLASHER_FISH = Registry.register(
                 Registry.ITEM,
                 new Identifier(ThrowableFluids.MOD_ID, "splasher_fish"),
-                new Item(new Item.Settings().food(ModFoodComponents.SPLASHER_FISH).group(ThrowableFluids.TF_ITEM_GROUP))
+                new Item(new Item.Settings().food(ModFoodComponents.SPLASHER_FISH).group(ItemGroup.FOOD))
         );
 
         SPLASHER_BUCKET = Registry.register(
                 Registry.ITEM,
                 new Identifier(ThrowableFluids.MOD_ID, "splasher_bucket"),
-                new EntityBucketItem(ModEntityTypes.SPLASHER_ENTITY, Fluids.WATER, SoundEvents.ITEM_BUCKET_EMPTY_FISH, (new Item.Settings()).maxCount(1).group(ThrowableFluids.TF_ITEM_GROUP))
+                new EntityBucketItem(ModEntityTypes.SPLASHER_ENTITY, Fluids.WATER, SoundEvents.ITEM_BUCKET_EMPTY_FISH, (new Item.Settings()).maxCount(1).group(ItemGroup.MISC))
         );
     }
 
@@ -63,13 +63,13 @@ public class ModItems {
         SPLASHER_SPAWN_EGG = Registry.register(
                 Registry.ITEM,
                 new Identifier(ThrowableFluids.MOD_ID, "splasher_spawn_egg"),
-                new SpawnEggItem(ModEntityTypes.SPLASHER_ENTITY, 0xaa9cff, 0xdcac55, new Item.Settings().group(ThrowableFluids.TF_ITEM_GROUP))
+                new SpawnEggItem(ModEntityTypes.SPLASHER_ENTITY, 0xaa9cff, 0xdcac55, new Item.Settings().group(ItemGroup.MISC))
         );
 
         GLOW_WORM_SPAWN_EGG = Registry.register(
                 Registry.ITEM,
                 new Identifier(ThrowableFluids.MOD_ID, "glow_worm_spawn_egg"),
-                new SpawnEggItem(ModEntityTypes.GLOW_WORM_ENTITY, 0xa73b3f, 0xff7c1b, new Item.Settings().group(ThrowableFluids.TF_ITEM_GROUP))
+                new SpawnEggItem(ModEntityTypes.GLOW_WORM_ENTITY, 0xa73b3f, 0xff7c1b, new Item.Settings().group(ItemGroup.MISC))
         );
     }
 }
--- src/main/java/sirjain/throwable_fluids/main/ThrowableFluids.java
+++ src/main/java/sirjain/throwable_fluids/main/ThrowableFluids.java
@@ -1,18 +1,12 @@
 package sirjain.throwable_fluids.main;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
 import sirjain.throwable_fluids.entity.other.ModEntitySpawn;
 import sirjain.throwable_fluids.entity.other.ModEntityTypes;
 import sirjain.throwable_fluids.items.ModItems;
-import java.util.Random;
 
 public class ThrowableFluids implements ModInitializer {
 	public static final String MOD_ID = "throwablefluids";
-	public static final ItemGroup TF_ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "throwable_fluids_item_group"), () -> new ItemStack(ModItems.THROWABLE_WATER));
 
 	@Override
 	public void onInitialize() {
