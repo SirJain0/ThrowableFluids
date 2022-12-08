be6848bf79be2afe2f71fe5b390ee2bb24001c2b
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-12-08 14:58:13 -0500

    Add translation and fix wrong sound ID

--- src/main/java/sirjain/throwable_fluids/sounds/ModSoundEvents.java
+++ src/main/java/sirjain/throwable_fluids/sounds/ModSoundEvents.java
@@ -7,7 +7,7 @@ import net.minecraft.util.registry.Registry;
 public class ModSoundEvents {
     public static final Identifier FLOP_SOUND_ID = new Identifier("throwablefluids:splasher_flop");
     public static final Identifier DEATH_SOUND_ID = new Identifier("throwablefluids:splasher_death");
-    public static final Identifier GLOW_WORM_DEATH_SOUND_ID = new Identifier("throwablefluids:splasher_death");
+    public static final Identifier GLOW_WORM_DEATH_SOUND_ID = new Identifier("throwablefluids:glow_worm_death");
 
     public static SoundEvent SPLASHER_FLOP = new SoundEvent(FLOP_SOUND_ID);
     public static SoundEvent SPLASHER_DEATH = new SoundEvent(DEATH_SOUND_ID);
