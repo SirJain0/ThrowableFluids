50f1b9340f1f364edbef95169d6ed89d1c8a2505
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-04-06 21:00:35 -0400

    Add back sounds

--- src/main/java/sirjain/throwable_fluids/main/ThrowableFluids.java
+++ src/main/java/sirjain/throwable_fluids/main/ThrowableFluids.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import sirjain.throwable_fluids.entity.other.ModEntitySpawn;
 import sirjain.throwable_fluids.entity.other.ModEntityTypes;
 import sirjain.throwable_fluids.items.ModItems;
+import sirjain.throwable_fluids.sounds.ModSoundEvents;
 
 public class ThrowableFluids implements ModInitializer {
 	public static final String MOD_ID = "throwablefluids";
@@ -16,6 +17,7 @@ public class ThrowableFluids implements ModInitializer {
 		ModEntityTypes.registerEntityRender();
 		ModEntityTypes.registerAttributes();
 		ModEntitySpawn.entitySpawn();
+		ModSoundEvents.registerSoundEvents();
 
 		System.out.println("Hi, "+MOD_ID+"!");
 	}
